a02d43c8a26e5b65c7d1d38dedd9b2f84a86b080
Alexander <alexander.i.kamenev@mail.com>
2017-06-07 00:27:26 +0300

    make home work

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # Sites Monitoring Utility
 
-[TODO. There will be project description]
+Assume you have file foo.txt with urls - new url in new line.
+
+For example
+```
+http://ya.ru
+https://google.com
+https://devman.org
+```
+Run 
+```
+python3 check_sites_health.py foo.txt 
+```
+Will show
+```python
+{'IS NOT EXPIRED FOR MONTH': True, 'URL': 'http://ya.ru', 'IS 200 OK': True}
+{'IS NOT EXPIRED FOR MONTH': True, 'URL': 'https://google.com', 'IS 200 OK': True}
+{'IS NOT EXPIRED FOR MONTH': True, 'URL': 'https://devman.org', 'IS 200 OK': True}
+```
+
+You should install requirements python3 -m pip install requirements.txt in vitual enviroment.
 
 # Project Goals
 
--- check_sites_health.py
+++ check_sites_health.py
@@ -2,12 +2,14 @@ import whois
 import requests
 from datetime import datetime, timedelta
 from urllib.parse import urlparse
+import argparse
 
 
 def load_urls4check(path, min_days_for_expiration):
     delta_expiration = timedelta(days=min_days_for_expiration)
     parsed_url = urlparse(path)
     return {
+        'URL': path,
         'IS 200 OK': is_server_respond_with_200(path),
         'IS NOT EXPIRED FOR MONTH': get_domain_expiration_date(parsed_url.hostname) - datetime.now() > delta_expiration,
     }
@@ -20,15 +22,20 @@ def is_server_respond_with_200(url):
 def get_domain_expiration_date(domain_name):
     expiration_date = whois.whois(domain_name).expiration_date
     if isinstance(expiration_date, list):
-        return whois.whois(domain_name).expiration_date[0]
+        return expiration_date[0]
     else:
-        return whois.whois(domain_name).expiration_date
+        return expiration_date
+
+
+def get_args():
+    parser = argparse.ArgumentParser(description='Check site health')
+    parser.add_argument('input', type=str, help='path where to find list of urls')
+    return parser.parse_args()
 
 if __name__ == '__main__':
-    list_urls = [
-        'http://ya.ru',
-        'https://google.com',
-        'https://devman.org',
-    ]
-    for url in list_urls:
+    args = get_args()
+    for line in open(args.input):
+        url = line.strip()
+        if not url:
+            continue
         print(load_urls4check(url, 30))
